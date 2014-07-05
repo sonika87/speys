64443136389ba1327c7f5d217114a3d0f7b4a032
Gaby <Gaby@Gaby-PC.lan>
2014-07-05 14:52:49 -0500

    Se actualizó paquetes LoginBean.java LoginDao.java LoginGestor.java LoginServlet.java Se actualizo: web.xml

---
+++ src/java/com/speys/Login/Bean/LoginBean.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.speys.Login.Bean;
+
+/**
+ *
+ * @author sonika
+ */
+public class LoginBean {
+    
+    private String nombreUsuario;
+
+    /**
+     * @return the nombreUsuario
+     */
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    /**
+     * @param nombreUsuario the nombreUsuario to set
+     */
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+    
+}
